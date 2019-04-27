04b8edc9b823e1cedd00c1b89973aeb69e44b2be
Yang Li-Yi <yangliyi1228@gmail.com>
2019-04-27 21:19:54 +0800

    Allow user to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,30 @@ class SessionsController < ApplicationController
 
   def new; end
 
+  def create
+    if user && correct_password?
+      sign_in(user)
+      flash[:notice] = 'Successfully logged in!'
+      redirect_to profile_path
+    else
+      flash[:alert] = 'email or password is not correct'
+      redirect_to login_path
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     flash[:notice] = 'Successfully logged out!'
     redirect_to login_path
+  end
+
+  private
+
+  def user
+    @user ||= User.find_by(email: params[:email])
+  end
+
+  def correct_password?
+    BCrypt::Password.new(user.encrypted_password) == params[:password]
   end
 end
